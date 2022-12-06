9038155c1bf1800399cd47ff3cd54c99c431732f
linhgiac <linhgiac@MacBook-Pro-2.local>
2022-12-07 00:40:07 +0700

    client: background component

---
+++ components/Background.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Pressable, Text, StyleSheet, View } from 'react-native';
+import { COLOR } from '../../constants';
+
+const Background = (props) => {
+    const { children, style } = props;
+
+    return <View style={[styles.background, style]}>{children}</View>;
+};
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        borderRadius: 6,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.41,
+        padding: 10,
+        margin: 10,
+        backgroundColor: COLOR.common.lightGrey,
+    },
+});
+export default Background;
